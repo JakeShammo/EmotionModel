1fbf9df0e3688e593b9c3ef1a9bf367977d829aa
JakeShammo <iamjacobzz@outlook.com>
2018-05-15 01:02:46 +0600

    Response generation roughly done

--- src/EmoBot.java
+++ src/EmoBot.java
@@ -9,30 +9,34 @@ import static java.lang.Math.sqrt;
 
 public class EmoBot {
     int goal;
-    public String receivedEmoState, currentEmoState;
-    double friendliness, currentLevel, transition, prevReceivedEmoState, currentDesire, coolingDownEffect = .2;
+    String receivedEmoState;
+    int receivedEmoIndex, currentLevel;
+    double friendliness, transition, prevReceivedEmoState, coolingDownEffect = .2;
     double[] levels = {0.0,0.0,0.0};
-    //double[] levelFactors = {.2, }
-    double[][] emotionValues = {{5,-5,-5},{3,-3,-3},{2.5,-2.5,-2.5},{2,-3,0},{2,-7,-3},{-2,4,-1},{-2,4,-2},{-5,6,4},{-6,7,5},{0,0,0}};
+    double[] levelFactors = {.1,0,.2 };
+    double[][] emotionValues = {{5,-5,-5},{3,-3,-3},{2.5,-2.5,-2.5},{2,-3,0},{2,-7,-3},{-2,4,-1},{-2,4,-2},{-5,2,4},{-6,2,5},{0,0,0}};
     List<String> emotions = Arrays.asList("Amusement", "Happiness", "Surprise", "Inspiration", "Sympathy", "Sadness", "Fear", "Anger", "Disgust", "Neutral");
-    //int[][] response = {{1,1,9,9},{1,9,9,7},{1,1,9,9},{1,1,9,7},{2,1,9,9},{2,1,9}};
+    int[][] responseValue = {{1,1,9},{1,1,9},{1,1,9},{1,1,7},{2,1,9},{4,4,9},{3,3,9},{5,6,7},{5,8,7},{2,9,9}};
+    List<String> response = Arrays.asList("Amusement", "Happy", "Surprise", "Inspiring", "Sympathy", "Sad", "Fear", "Anger", "Disgust", "Neutral");
     EmoBot(int goal, float friendliness){
         this.friendliness = friendliness;
         this.goal = goal;
+        transition = 0;
     }
     EmoBot(){
+        transition = 0;
     }
     String geterateOverallEmotion(){
-        int emoIndex = emotions.indexOf(receivedEmoState);
-        if(emoIndex == -1) emoIndex = 9;
+        if(receivedEmoIndex == -1) {
+            receivedEmoIndex = 9;
+            System.out.println("Unknown Emotion");
+        }
         //System.out.println(emoIndex);
         double maxVal = 0 ;
         int maxLevel = -1;
         String state = "neutral";
         for(int i=0;i<3;i++){
-            if(i==0) levels[i] += emotionValues[emoIndex][i];
-            else if(i==2) levels[i] += emotionValues[emoIndex][i];
-            else levels[i] += emotionValues[emoIndex][i];
+            levels[i] += emotionValues[receivedEmoIndex][i] - levelFactors[i]*levels[i];
             levels[i] = min(levels[i],20);
             if(levels[i] != 0) levels[i] -= coolingDownEffect + i*.5;
             levels[i] = max(0,levels[i]);
@@ -66,6 +70,24 @@ public class EmoBot {
             else state = "Angry";
         }
         return state;
+    }
+
+    public String geterateResponse(){
+        if(transition==1){
+            transition = 0;
+            return "Surprise";
+        }
+        return response.get(responseValue[receivedEmoIndex][currentLevel]);
+
+    }
+
+    public void setReceivedEmoState(String receivedEmoState) {
+        this.receivedEmoState = receivedEmoState;
+        receivedEmoIndex = emotions.indexOf(receivedEmoState);
+        if(prevReceivedEmoState == 8 || prevReceivedEmoState == 9){
+            if(receivedEmoIndex<3) transition = 1;
+        }
+        prevReceivedEmoState = receivedEmoIndex;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         EmoBot emoBot = new EmoBot();
+        System.out.println("Rough emoBot model");
+        System.out.println("Each time input one of the following emotions:");
+        System.out.println("Amusement, Happiness, Surprise, Inspiration, Sympathy, Sadness, Fear, Anger, Disgust, Neutral");
         while(true){
             Scanner scanner = new Scanner(System. in);
+            System.out.print("Input Emotion: ");
             String input = scanner. nextLine();
             if(input.equalsIgnoreCase("Exit")) break;
-            emoBot.receivedEmoState = input;
-            System.out.println(emoBot.geterateOverallEmotion());
+            emoBot.setReceivedEmoState(input);
+            System.out.println("Overall State: "+emoBot.geterateOverallEmotion());
+            System.out.println("Response Type: "+emoBot.geterateResponse());
         }
     }
+
 }
